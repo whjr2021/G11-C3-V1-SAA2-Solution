34aca836285470ff1088681c180d5d6042b873fd
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-10-18 13:54:05 +0530

    Add files via upload

---
+++ C3_SAA2_Solution.py
@@ -0,0 +1,20 @@
+# Define a variable "country" and assign the country name
+country = 'India'
+
+# Define a variable "age" and assign an appropriate age value
+age = 18
+
+# Check for the eligibility to vote in India
+# If "age" is less than 18, print "Not eligible to vote"
+if age < 18:
+    print("Not eligible to vote")
+
+# If "country" is not India, print "Not eligible to vote in India"  
+if country != "Indian":
+    print("Not eligible to vote in India")
+
+# If "country" is India and "age" is greater or equal to 18, print "Eligible to vote in India"    
+if country == 'India':
+    if age >= 18:
+        print("Eligible to vote in India")
+
