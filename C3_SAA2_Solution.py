cf8dab780fd0dbee69547d1f176d8988ac51ee00
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-10-22 10:51:39 +0530

    Update C3_SAA2_Solution.py

--- C3_SAA2_Solution.py
+++ C3_SAA2_Solution.py
@@ -10,11 +10,10 @@ if age < 18:
     print("Not eligible to vote")
 
 # If "country" is not India, print "Not eligible to vote in India"  
-if country != "Indian":
+if country != "India":
     print("Not eligible to vote in India")
 
 # If "country" is India and "age" is greater or equal to 18, print "Eligible to vote in India"    
 if country == 'India':
     if age >= 18:
         print("Eligible to vote in India")
-
